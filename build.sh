f22a34403c3415295434489e304ef8a3b59f16eb
chapa0723 <dchaparro0723@gmail.com>
2025-11-19 02:10:18 -0300

    otro cambio en el build

--- build.sh
+++ build.sh
@@ -7,10 +7,10 @@ pip install psycopg2-binary
 python manage.py makemigrations 
 python manage.py migrate 
 # definir credenciales de superusuario
-#export DJANGO_SUPERUSER_USERNAME=david 
-#export DJANGO_SUPERUSER_EMAIL=david@david.com
-#export DJANGO_SUPERUSER_PASSWORD=david
+export DJANGO_SUPERUSER_USERNAME=david2 
+export DJANGO_SUPERUSER_EMAIL=david2@david.com
+export DJANGO_SUPERUSER_PASSWORD=david2
 # crear superusuario
-#python manage.py createsuperuser --no-input
+python manage.py createsuperuser --no-input
 
 
